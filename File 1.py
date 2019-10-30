8ce736c847d2a93f12604ba09eeb7482167cfd6c
Chuchelov <chucheloff@gmail.com>
2019-10-30 09:50:23 +0300

    great, we making progress. let's test it on my mak

--- File 1.py
+++
@@ -1,2 +0,0 @@
-trey = input("Zavozy Gavna " )
-print("\n"+ (trey.replace("п","г")+" ") * 10)
---
+++ logtutor.py
@@ -0,0 +1,58 @@
+import logging
+
+print('welcome to logger - bogger wtf is # NOTE: doing in here')
+
+# THOSE ARE ATOM BUILT-IN HELPERS:
+# NOTE: well, where does it go
+# WARNING: why you gotta do me like that
+# TODO: this might be helpful in some cases
+
+
+# LOGGER LEVELS FOR logging.basicConfig(level,force) setup
+# CRITICAL = 50
+# ERROR = 40
+# WARNING = 30
+# INFO = 20
+# DEBUG = 10
+# NOTSET = 0
+
+0
+print('\nsetting NOTSET level\n')
+logging.basicConfig( level = logging.NOTSET )
+logging.warning('HeLP')
+logging.debug('greta van fleet')
+logging.info('logging info level')
+logging.error('nice')
+logging.log(msg = 'just log, okay?', level = logging.NOTSET)
+
+print('\nsetting ERROR level\n')
+logging.basicConfig( level = logging.ERROR, force = True)
+logging.warning('HeLP')
+logging.debug('greta van fleet')
+logging.info('logging info level')
+logging.error('nice')
+logging.log(msg = 'just log, okay?', level = logging.NOTSET)
+
+print('\nsetting DEBUG level\n')
+logging.basicConfig( level = logging.DEBUG)
+logging.warning('HeLP')
+logging.debug('greta van fleet')
+logging.info('logging info level')
+logging.error('nice')
+logging.log(msg = 'just log, okay?', level = logging.NOTSET)
+
+print('\nsetting WARNING level\n')
+logging.basicConfig( level = logging.WARNING)
+logging.warning('HeLP')
+logging.debug('greta van fleet')
+logging.info('logging info level')
+logging.error('nice')
+logging.log(msg = 'just log, okay?', level = logging.NOTSET)
+
+print('\nsetting INFO level\n')
+logging.basicConfig( level = logging.INFO)
+logging.warning('HeLP')
+logging.debug('greta van fleet')
+logging.info('logging info level')
+logging.error('nice')
+logging.log(msg = 'just log, okay?', level = logging.NOTSET)
