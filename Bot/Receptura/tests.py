7fb2306afbfc77c4b4ee031ffa2fecadd8271509
Чучелов Никита <n.chuchelov@betplanet.com>
2020-10-14 21:04:17 +0300

    what

--- Bot/Receptura/tests.py
+++ Bot/Receptura/tests.py
@@ -1,30 +1,3 @@
-from time import sleep
-import time
-
-with open('C:/Users/Tom/Documents/Python/Bot/Receptura/deputy_wait_queue.txt','w') as f:
-    f.write('empty')
-
-'''
-with open('deputy_wait_queue.txt','w') as f:
-    f.write('548993 ')
-    f.write(str(time.time())+'\n')
-    sleep(2)
-    f.write('548993 ')
-    f.write(str(time.time())+'\n')
-    sleep(2)
-    f.write('358893 ')
-    f.write(str(time.time())+'\n')
-
-
-with open('deputy_wait_queue.txt','r') as f:
-    queue = f.read().split('\n')
-wait_list = []
-for item in queue:
-    wait_list.append(item.split(' '))
-
-
-sleep(3)
-then = time.time()
-difference = int(then-now)
-print(difference)
-'''
+with open('Python/Bot/Receptura/deputy_wait_queue.txt','r') as f:
+    a = f.read()
+    print(a)
